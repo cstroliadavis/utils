926cb7b627259109b4a7978b885d504d584a178f
Chris <full-stack.dev@strolia-davis.us>
2024-03-07 18:16:05 -0500

    Add some minor refactoring and documentation

--- src/array/after/after.mjs
+++
@@ -1,9 +0,0 @@
-const OVERLOAD = {
-  number: (array, number) => array.slice(number),
-  string: (array, string) => array.slice(array.findIndex((value) => value > string)),
-  'function': (array, fn, comp) => array.slice(array.findIndex((value) => fn(value, comp) > 0)),
-};
-
-const after = (array, value, ...args) => OVERLOAD[typeof value]?.(array, value, ...args);
-
-export default after;
